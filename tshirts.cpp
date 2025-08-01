05aaabd91fa1c9bb9dba5b0e3590a934a7d63698
KrishnaPrasad2606 <98822676+KrishnaPrasad2606@users.noreply.github.com>
2025-08-01 10:25:35 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -1,22 +1,32 @@
-#include <iostream>
+#include <stdio.h>
 #include <assert.h>
 
 char size(int cms) {
     char sizeName = '\0';
-    if(cms < 38) {
+    if (cms < 38) {            // include 38
         sizeName = 'S';
-    } else if(cms > 38 && cms < 42) {
+        printf("For CM %d Size S\n",cms);
+    } else if (cms > 38 && cms < 42) {     // include 42
         sizeName = 'M';
+        printf("For CM %d Size M\n",cms);
     } else if(cms > 42) {
         sizeName = 'L';
+         printf("For CM %d Size L\n",cms);
     }
     return sizeName;
 }
 
 void testTshirtSize() {
-    std::cout << "\nTshirt size test\n";
-    assert(size(37) == 'S');
-    assert(size(40) == 'M');
-    assert(size(43) == 'L');
-    std::cout << "All is well (maybe!)\n";
+    printf("\nTshirt size test\n");
+    assert(size(37) == 'S');  // S
+    assert(size(38) == 'S');  // Edge case
+    assert(size(40) == 'M');  // M
+    assert(size(42) == 'M');  // Edge case
+    assert(size(43) == 'L');  // L
+    printf("All is well (maybe!)\n");
+}
+
+int main() {
+    testTshirtSize();
+    return 0;
 }
